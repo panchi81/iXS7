31b162b220b2ea8cdfacca246015ceb613761cae
Panchi <francisco.gonzalez.costa@gmail.com>
2021-09-27 01:42:42 +0200

    minor mods

--- read_csv.py
+++ read_csv.py
@@ -8,7 +8,7 @@ from string import digits
 # readfile_path = Path("removed_tags.txt")
 readfile_path = Path("iX_Export.txt")
 writefile_path = Path("Data_Block_export.scl")
-delimiter = ""
+delimiter = ","
 
 data_types = {
     "DEFAULT": [0, ""],
@@ -18,19 +18,47 @@ data_types = {
     "UINT16": [0, ""],
     "INT32": [0, ""],
     "UINT32": [0, ""],
-    "DOUBLE": [0, ""],
     "FLOAT": [0, "Real"],
-    "DATETIME": [0, ""],
+    "DOUBLE": [0, ""],
     "STRING": [0, "String"],
+    "DATETIME": [0, ""],
 }
 
 db = []
+# DEBUG
+# print("Testing")
 
-
-with open(readfile_path, "r") as csv_file:
-    reader = csv.DictReader(csv_file, delimiter=",")
+with open(readfile_path, "r") as ix_tag_export, open(writefile_path, "a") as s7_db_scl:
+    # initiate cleaning of header
+    h_reader = csv.reader(ix_tag_export)
+    header = next(h_reader)
+    header[0] = header[0].removeprefix("// ")
+    header[-1] = header[-1].removesuffix(" //")
+    reader = csv.DictReader(ix_tag_export, fieldnames=header, delimiter=delimiter)
+    # skip the header
+    next(reader)
     for row in reader:
-        print(row)
+        # Determine the Siemens Address Column
+        for key in row:
+            if row[key].startswith("DB"):
+                Address = key
+                break
+        # create list of included DataBlocks
+        snippet = row[Address].split(".")[0]
+        if snippet.startswith("DB"):
+            db.append(snippet)
+    for xrow in reader:
+        print(xrow)
+    # prepare DataBlock dict
+    db_dict = {i: [] for i in sorted(set(db))}
+    # DEBUG
+    # print(db_dict)
+
+
+# ToDo: Parse iX export file,
+# ToDo: Sort into respective S7 DB
+# ToDo: Fill-in missing DB entries with expected (missing) datatype and enumerated Tag_name.
+# ToDo: Write output file
 
 # with open(readfile_path, "r") as csv_file:
 #     reader = csv.reader(csv_file, delimiter=",")
@@ -39,8 +67,8 @@ with open(readfile_path, "r") as csv_file:
 #         if row[2].split(".")[0][0:2] == "DB":
 #             db.append(row[2].split(".")[0])
 
-unique_db = set(db)
-db_dict = {i: [] for i in unique_db}
+# unique_db = set(db)
+# db_dict = {i: [] for i in unique_db}
 
 # # total = sum(data_types.values())
 # # for k,v in data_types.items():
@@ -87,3 +115,8 @@ db_dict = {i: [] for i in unique_db}
 #                 f"      {val.replace('.', '_')} : {s7_datatypes[val.split('.')[1].rstrip(digits)]};\n"
 #             )
 #         f_out.write(part2)
+
+# # Report
+# print(f"Constructed {s7_db_scl} containing:")
+# print(f"{number_of_dbs} DataBlocks")
+# print(f"{number_of_entries} DataBlock entries (Tags)")
