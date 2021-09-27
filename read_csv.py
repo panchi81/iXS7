c7e3a9bbda77d0899d0aeafcb70ed24bd0d2b941
FRAGO <frago@beijerelectronics.com>
2021-09-27 15:46:47 +0200

    trying to data type

--- read_csv.py
+++ read_csv.py
@@ -7,7 +7,7 @@ from string import digits
 # readfile_path = Path("C:/Users/FRAGO/Documents/Support2021/178095 - S7ISOTCP_recipe items missing/Export/export.txt")
 # readfile_path = Path("export.txt")
 # readfile_path = Path("removed_tags.txt")
-readfile_path = Path("iX_Export.txt")
+readfile_path = Path("test_export.txt")
 writefile_path = Path("Data_Block_export.scl")
 delimiter = ","
 
@@ -16,13 +16,12 @@ data_types = {
     "BIT": [0, "Bool"],
     "BOOL": [0, "Bool"],
     "INT16": [0, "Word"],
-    "UINT16": [0, ""],
-    "INT32": [0, ""],
-    "UINT32": [0, ""],
+    "UINT16": [0, "UInt"],
+    "INT32": [0, "DWord"],
+    "UINT32": [0, "UDInt"],
     "FLOAT": [0, "Real"],
-    "DOUBLE": [0, ""],
+    "DOUBLE": [0, "LReal"],
     "STRING": [0, "String"],
-    "DATETIME": [0, ""],
 }
 
 db = []
@@ -32,36 +31,53 @@ db = []
 with open(readfile_path, "r") as ix_tag_export, open(writefile_path, "a") as s7_db_scl:
     # initiate cleaning of header
     h_reader = csv.reader(ix_tag_export)
+    # skip header
     header = next(h_reader)
+    # clean up header
     header[0] = header[0].removeprefix("// ")
     header[-1] = header[-1].removesuffix(" //")
+
     # Work with the following reader
     reader = csv.DictReader(ix_tag_export, fieldnames=header, delimiter=delimiter)
     # skip the header
     next(reader)
+
+    # Determine the Siemens Address Column
     for row in reader:
-        # Determine the Siemens Address Column
         for key in row:
             if row[key].startswith("DB"):
-                Address = key
+                s7_address = key
                 break
+
         # create list of included DataBlocks
-        snippet = row[Address].split(".")[0]
-        if snippet.startswith("DB"):
-            db.append(snippet)
+        data_block = row[s7_address].split(".")[0]
+        if data_block.startswith("DB"):
+            db.append(data_block)
+
     # prepare DataBlock dict
-    db_dict = {i: [] for i in sorted(set(db))}
+    # Sorted by DB
+    # db_dict = {i: [] for i in sorted(set(db))}
+    # unsorted dictionary
+    db_dict = {i: [] for i in set(db)}
+
     # read from the top
     ix_tag_export.seek(0)
-    # skip header (again)
+    # skip header
     next(reader)
-    # Debugs
-    for x_row in reader:
-        print(x_row)
 
+    # populate DataBlock (keys) dictionary with list of Siemens S7 register addresses (values)
+    for row in reader:
+        db_dict.setdefault(row[s7_address].split(".")[0], []).append(row[s7_address])
 
-# ToDo: Parse iX export file,
-# ToDo: Sort into respective S7 DB
+    # sort primarily by DB (key), and secondarily by register address
+    sorted_tags = {
+        i: sorted(db_dict[i], key=lambda x: int(x.split(".")[1][3:])) for i in db_dict
+    }
+    # Debug
+    print(f"{db_dict= }")
+    print(f"{sorted_tags= }")
+
+# ToDo: Filter out addresses within other addresses (Bit in word/float/long, word in float/long, or float in long)
 # ToDo: Fill-in missing DB entries with expected (missing) datatype and enumerated Tag_name.
 # ToDo: Write output file
 
